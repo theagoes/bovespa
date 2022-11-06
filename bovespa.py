7018c9c58e2989bdc067774dbbcc5fa424974e59
Anderson Goes Silva <agoes@outlook.com>
2022-11-06 20:53:22 -0300

    Ref004

--- bovespa.py
+++ bovespa.py
@@ -2,18 +2,13 @@ from pandas_datareader import data as web
 import pandas as pd
 import plotly.express as px
 import streamlit as st
+import openpyxl
+from style import hide_st_style
 
 tabela_empresas =  pd.read_excel("empresas.xlsx")
 
 st.set_page_config(page_title="Cotações B3 Web",page_icon=":bar_chart:")
 st.sidebar.header("Filtros")
-
-hide_st_style = """ 
-                <style>
-                #MainMenu {visibility: hidden;}
-                footer {visibility: hidden;}
-                </style>
-                """
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
 empresa = st.sidebar.selectbox('Empresas', tabela_empresas)
---
+++ style.py
@@ -0,0 +1,6 @@
+hide_st_style = """ 
+                <style>
+                #MainMenu {visibility: hidden;}
+                footer {visibility: hidden;}
+                </style>
+                """
