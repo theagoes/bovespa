ba4ecbe7cadfc9e8ea1adf0ccfbab29fee4c5568
Anderson Goes Silva <agoes@outlook.com>
2022-11-07 10:11:39 -0300

    ref013

--- bovespa.py
+++ bovespa.py
@@ -2,10 +2,9 @@ from pandas_datareader import data as web
 import pandas as pd
 import plotly.express as px
 import streamlit as st
-import openpyxl
 from style import hide_st_style
 
-tabela_empresas =  pd.read_excel("Empresas.xlsx")
+tabela_empresas =  ['USIM3','USIM5','VALE3','PETR3','PETR4','BBDC4']
 
 st.set_page_config(page_title="Cotações B3 Web",page_icon=":bar_chart:")
 st.sidebar.header("Filtros")
@@ -18,7 +17,7 @@ st.markdown("##")
 cotacao = web.DataReader(f'{empresa}.SA', data_source='yahoo',  start="01/01/2021", end = "12/31/2022")
 cotacao_red = cotacao.drop(cotacao.columns[[0,1,4]], axis = 1)
 
-fig_bar = px.line(cotacao_red)
+fig_bar = px.line(cotacao_red, template= 'plotly_white')
 st.plotly_chart(fig_bar)
 st.write(cotacao)
     
